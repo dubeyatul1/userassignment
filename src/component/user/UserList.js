5d3c9cf1360ff117431690715bdf392e223dfb08
ss251326 <ss251326@ncr.com>
2018-11-08 22:43:32 +0530

    Changed base url

--- src/component/user/UserList.js
+++ src/component/user/UserList.js
@@ -54,7 +54,7 @@ class UserList extends Component {
               <td>{user.phone}</td>
               <td>{user.address}</td>
               <td>
-                <i className="fa fa-times" aria-hidden="true" onClick={(e) => this.handleDelete(user._id)}></i>
+                {(user.email !== 'admin@gmail.com')?(<i className="fa fa-times" aria-hidden="true" onClick={(e) => this.handleDelete(user._id)}></i>):<i class="fa fa-ban" aria-hidden="true"></i>}
               </td>
             </tr>
             ))
