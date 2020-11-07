32cd912fd8e1ae1df4d43557029fe520e52e9898
dubeyatul1 <61591023+dubeyatul1@users.noreply.github.com>
2020-11-07 16:51:56 +0530

    Updated base url

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,2 +1,3 @@
 // export const baseUrl = 'http://localhost:8100/';
-export const baseUrl = 'https://nodemongorestapi.herokuapp.com/';
+//export const baseUrl = 'https://nodemongorestapi.herokuapp.com/';
+export const baseUrl = 'https://userassignmentapi.herokuapp.com';
